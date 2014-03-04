7a75b8b9b2f2ff5ee43b88af265d16da4aafd5d0
Manuel Gil <gikdew@gmail.com>
2014-03-04 13:51:01 +0100

    setImage();
    Coge el links de cada la imagen de cada videos.
    Note --> Hacer que elija la calidad de la imagen.

--- src/com/example/rss/XMLParser.java
+++ src/com/example/rss/XMLParser.java
@@ -3,6 +3,7 @@ package com.example.rss;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
+
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 
@@ -17,57 +18,68 @@ import android.util.Log;
 public class XMLParser {
 	private URL url;
 	Context contextor;
-	
-	public XMLParser(String url,Context contexto) {
-		contextor=contexto;
+
+	public XMLParser(String url, Context contexto) {
+		contextor = contexto;
 		try {
 			this.url = new URL(url);
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
 		}
 	}
-	
-	public ArrayList<Video> parse() {
-		
+
+	public ArrayList < Video > parse() {
+
 		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-		ArrayList<Video> Videos = new ArrayList<Video>();
+		ArrayList < Video > Videos = new ArrayList < Video > ();
 		Video Video;
-		
-		
+
 		try {
 			DocumentBuilder builder = factory.newDocumentBuilder();
 			Document dom = builder.parse(this.url.openConnection().getInputStream());
 			Element root = dom.getDocumentElement();
 			NodeList items = root.getElementsByTagName("item");
-			for (int i=0;i<items.getLength();i++){
-				Video = new Video();				
+			for (int i = 0; i < items.getLength(); i++) {
+				Video = new Video();
 				Node item = items.item(i);
 				NodeList properties = item.getChildNodes();
-				for (int j=0;j<properties.getLength();j++){
+				//Log.i("NodeList", properties.toString());
+				for (int j = 0; j < properties.getLength(); j++) {
 					Node property = properties.item(j);
 					String name = property.getNodeName();
-					if (name.equalsIgnoreCase("title")){
+					//Log.i("name", name);
+					if (name.equalsIgnoreCase("title")) {
 						Video.setTitle(property.getFirstChild().getNodeValue());
-					}else if (name.equalsIgnoreCase("content:encoded")){
-												
-					}else if (name.equalsIgnoreCase("description")){
-						
-					}else if (name.equalsIgnoreCase("link")){
-						Video.setLink(property.getFirstChild().getNodeValue());						
-					}else if(name.equalsIgnoreCase("pubDate")){
-						
+					} else if (name.equalsIgnoreCase("media:group")) {
+						NodeList medList = property.getChildNodes();
+						for (int w = 0; w < medList.getLength(); w++) {
+							Node property1 = medList.item(w);
+							String name1 = property1.getNodeName();
+							if (name1.equalsIgnoreCase("media:thumbnail")) {
+								Element e = (Element) property1;
+								String thumbUrl = e.getAttribute("url");
+								//Log.i("MediaThumbnail", thumbUrl);											
+								Video.setImage(thumbUrl);
+							}
+							//Log.i("name1", name1);
+						}
+					} else if (name.equalsIgnoreCase("description")) {
+
+					} else if (name.equalsIgnoreCase("link")) {
+						Video.setLink(property.getFirstChild().getNodeValue());
+					} else if (name.equalsIgnoreCase("pubDate")) {
+
 					}
-					
 				}
 				Videos.add(Video);
-				Log.i("Parsher", "video: "+ Video.getTitle());
+				Log.i("Parsher", "Video Image: " + Video.getImage());
 			}
-			
+
 		} catch (Exception e) {
 			throw new RuntimeException(e);
-		} 
+		}
 
 		return Videos;
 	}
-	
+
 }
